b8b98962b21b332c118187cd227788cbfe8ef458
Christian Cueni <christian.cueni@iterativ.ch>
2020-12-09 19:04:10 +0100

    Add modal component

---
+++ src/views/Modal.vue
@@ -0,0 +1,30 @@
+    <template>
+      <div>
+        <ion-header>
+          <ion-toolbar>
+            <ion-title>{{ title }}</ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-margin">
+          <div>
+          <h1>Hello</h1>
+          <p>test</p>
+          <p>test</p>
+          </div>
+
+        </ion-content>
+      </div>
+    </template>
+
+    <script>
+    import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/vue';
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+      name: 'Modal',
+      props: {
+        title: { type: String, default: 'Super Modal' },
+      },
+      components: { IonContent, IonHeader, IonTitle, IonToolbar }
+    });
+    </script>
